92f68c5db8fdd2c94ea97dd630574a607f3205dd
Manuj Sharma <technomanuj@hotmail.com>
2020-09-13 20:33:42 +0530

    Second Commit

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -24,6 +24,21 @@ namespace myweather.Controllers
         }
 
         [HttpGet]
+        [Route("GetWeather")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        [Route("GetWeatherNew")]
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
